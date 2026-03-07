a92d439cac0dee819f2ae29d3b38c80ede3a94d1
John Lhoel A. Morillo <johnlhoelmorillo8@gmail.com>
2026-03-08 07:22:16 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,5 @@
+let onAuthSuccess = null;
+
 const SUPABASE_URL = 'https://oikumdcokfhrzuvgmxku.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pa3VtZGNva2Zocnp1dmdteGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NzA2NTYsImV4cCI6MjA4ODQ0NjY1Nn0.X_PzXZswIFPKZddV24rcSql6PbVoR0vmuKdn3Xh_qAQ';
 
@@ -67,6 +69,7 @@ function showAuthScreen() {
 
 function hideAuthScreen() {
   document.getElementById('auth-overlay').style.display = 'none';
+  if (onAuthSuccess) { onAuthSuccess(); onAuthSuccess = null; }
 }
 
 async function handleLogin() {
